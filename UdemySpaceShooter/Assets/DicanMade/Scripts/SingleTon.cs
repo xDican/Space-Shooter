466b2246d87b92e85769b6c9eb7c97921a16969a
Dican <jdescalante1311@gmail.com>
2019-05-22 07:24:29 -0600

    Music

---
+++ UdemySpaceShooter/Assets/DicanMade/Scripts/SingleTon.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SingleTon : MonoBehaviour
+{
+    private void Awake() {
+        
+        if(FindObjectsOfType(GetType()).Length > 1)
+        {
+            Destroy(this.gameObject);
+        }
+        DontDestroyOnLoad(this.gameObject);
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
